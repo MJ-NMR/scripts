b23d5fbd5d53030a798c9034da16500c94ccc160
zaater <almogtbanmr123@gmail.com>
2025-12-17 21:38:59 -0500

    Rename suspend to sleep in power.sh

--- power.sh
+++ power.sh
@@ -4,12 +4,12 @@
 #export XAUTHORITY=/run/lightdm/zaater/xauthority
 
 
-choice=$(printf "Cancel\n  Shutdown\n󰑓  Reboot\n󰒲  Suspend\n  Lock\n󰗽  Logout" | rofi -dmenu -theme /home/zaater/.config/rofi/dmenu.rasi -theme-str 'window { width: 30%; }' -i -p "Power Menu")
+choice=$(printf "Cancel\n  Shutdown\n󰑓  Reboot\n󰒲  Sleep\n  Lock\n󰗽  Logout" | rofi -dmenu -theme /home/zaater/.config/rofi/dmenu.rasi -theme-str 'window { width: 30%; }' -i -p "Power Menu")
 
 case "$choice" in
   *Shutdown) systemctl poweroff ;;
   *Reboot) systemctl reboot ;;
-  *Suspend) systemctl suspend ;;
+  *Sleep) systemctl suspend ;;
   *Lock) i3lock ;;
   *Logout) i3-msg exit ;;
   *) exit 0 ;;
