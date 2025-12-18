eaba892d758c164bf9c87457ffb7b0a1f42b55e0
zaater <almogtbanmr123@gmail.com>
2025-12-17 23:53:05 -0500

    Add the Full battery case in battery-warner.sh

--- battery-warner.sh
+++ battery-warner.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 switch_xset() {
-	if [[ $status == "Charging" ]]; then
-		xset s 0 0
+	if [[ "$status" == "Charging" || "$status" == "Full" ]]; then
+		xset s off
 		xset -dpms
 		notify-send -i battery-good-charging "AC Power" "Chargering"
 		return
 	else
 		[[ "$battery_level" -le 10 ]] && notify-send -u critical -i battery-low "Battery Low" "Battery is at ${battery_level}%!"
-		xset s 1800 1800
+		#xset s 1800 1800
+		xset s on
 		xset +dpms
 		xset dpms 0 2700 2700
 		notify-send -i battery "AC Power" "Stop Charger"
@@ -18,7 +19,7 @@ switch_xset() {
 old=""
 while true; do
 	battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
-	status=$(acpi -b | grep -o 'Charging\|Discharging')
+	status=$(acpi -b | grep -o 'Charging\|Discharging\|Full')
 
 	echo "status $status old $old"
 	if [[ "$status" != "$old" ]]; then
